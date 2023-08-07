3b72380607a4c65343aff540c1ccdf3efcb6d676
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-08-07 18:17:23 +0200

    square wave, im almost sure it should work

--- sg/sg.h
+++ sg/sg.h
@@ -12,6 +12,16 @@ namespace sound {
         short result = amplitude * sin(rad);
         return result;
     }
+
+    short SquareWave(double time, double freq, double amp) {
+        double tpc = 44100 / freq;
+        double cycles = time / tpc;
+        double rad = 6.283185307 * cycles;
+        double amplitude = 32767 * amp;
+        double d = ((sin(rad) > 0) * 2) - 1;
+        short result = amplitude * d;
+        return result;
+    }
 }
 
 #endif
