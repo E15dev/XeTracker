97097903d6a3000070ce2d4fd26ffb3371623720
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-08-29 22:04:09 +0200

    new file format, new soundgen format

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
-import pickle
 from time import time
+import p2f
+from tx import OD
+from cf import readProj, saveProj
 
 sys.argv.pop(0)                                                 # pop path
 fp = sys.argv.pop(0)                                            # project name
@@ -10,8 +12,7 @@ for arg in sys.argv:
         pids.append(int(arg))                                   # args that left are patterns ids
 
 try:
-    f = open(fp, "rb")
-    cproj = pickle.load(f)
+    cproj = readProj(fp)
     if pids == []:                                               # if no patterns specified, it will play all not muted
         pids = range(len(cproj.patterns))
     tempo = cproj.getTempo()
@@ -23,9 +24,13 @@ try:
         d = ""
         for pi in pids:
             if not cproj.patterns[pi].muted:
-                d = d + " " + str(cproj.playerRead(pi, i))
+                # vol freq instrument automation
+                note = cproj.patterns[pi].notes[cproj.patterns[pi].playerIndex(i)]
+                d += " " + str(note.vol/255) + " " + str(p2f.convert(note.pitch)) + " " + str(cproj.patterns[pi].instrument) + " " + "0"
         sys.stdout.write(d[1:] + "\n")                          # first char is always space
         sys.stdout.flush()
         i = i + 1
+except AttributeError:
+    print(OD)
 except KeyboardInterrupt:
     pass
