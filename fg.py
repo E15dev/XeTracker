c7f022fc12a179301bcd1de5dc9ef4dcc1fed6b3
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-08-29 22:24:07 +0200

    after rename in hd

--- fg.py
+++ fg.py
@@ -1,12 +1,12 @@
 import hd
 
 def empty(count, name):
-    return hd.TrFile(count, name)
+    return hd.TrProject(count, name)
 
 def random(count, name):
     from random import random
     from math import floor
-    d = hd.TrFile(count, name)
+    d = hd.TrProject(count, name)
     for i in range(count):
         for j in range(64):
             d.write(i, j, floor(random()*12))
