e48911597f8a1b56232032e849e8a847838238a6
root <root@ACER-erwin>
2023-09-12 17:24:03 +0200

    using argv instead cin

--- src/cfv.cpp
+++ src/cfv.cpp
@@ -7,17 +7,20 @@
 
 void phex(uint8_t b) { printf("%02x ", b); }
 
-int main() {
-    std::string path;
-    getline(std::cin, path);
-    std::ifstream file(path, std::ios::in | std::ios::binary);
+int main(int argc, char *argv[0]) {
+    if (argc<2) {printf("you need to specify filename\n"); exit(0);}
+
+    std::ifstream file(argv[1], std::ios::in | std::ios::binary);
     if (!file.is_open()) {return 1;};
     std::istream_iterator<uint8_t> start(file), end;
     file >> std::noskipws;
     std::vector<uint8_t> data(start, end);
 
     cf::Project proj = cf::Project(data);
-//    std::cout << static_cast<uint16_t>(proj.getPattern(3).notes[1].pitch)<< std::endl;
+
+    for(int i = 0; i<data.size(); i++) { // just print all bytes from data as hex
+        phex(data[i]);
+    };
 
     return 0;
 }
