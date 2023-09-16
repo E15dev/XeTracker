73c2db867b136dc4e665cb6b28c19268a128bba0
E15dev <e15.rgbcubestudio@gmail.com>
2023-09-16 17:01:07 +0200

    changed names

--- src/sg.h
+++ src/sg.h
@@ -6,16 +6,17 @@
 #include <math.h>
 
 namespace waveforms {
-    short _WTData(double time, double freq, uint8_t data[255]) {
-        double tpc = 44100.0/freq;
-        return 256*static_cast<int8_t>(data[1+static_cast<int8_t>(floor(fmod(time/tpc, 1)*126))]); // 126 because single frame in wavetable is 127 bytes
+    double getSpc(double freq) { return 44100.0/freq; } // get samples per cycle
+
+    short WTData(double time, double freq, uint8_t data[255]) {
+        return 256*static_cast<int8_t>(data[1+static_cast<int8_t>(floor(fmod(time/getSpc(freq), 1)*126))]); // 126 because single frame in wavetable is 127 bytes
     }
 
     // WIP of generator from instrument
     short fromInstrument(double time, double freq, double amp, cf::Instrument instr) {
         short out;
         if (instr.type == 0x50) { return 0; } // "P", ignoring data, return 0
-        if (instr.type == 0x77) { out = _WTData(time, freq, instr.data); } // "w"
+        if (instr.type == 0x77) { out = WTData(time, freq, instr.data); } // "w"
         return out*amp;
     }
 }
